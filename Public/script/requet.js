2a5e7c3043ef41fa6fb3f0211ac78842765e6fa7
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-11-15 16:14:43 +0100

    final

--- Public/script/requet.js
+++ Public/script/requet.js
@@ -27,7 +27,7 @@ export function getDataForm()
     let shortDescription = document.getElementById('shortDescription');
     let data = {
         name: title.value,
-        description: description.editor.getDocument(),
+        description: description.editor.getDocument().toString(),
         shortDescription: shortDescription.value,
         image: image.src.replace(/^.+,/, '')
     };
